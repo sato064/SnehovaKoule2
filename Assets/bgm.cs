166e5bfb6e7d279c24a05bd4894854fc3df2dca8
sato064 <satoberu@gmail.com>
2020-02-06 19:51:15 +0900

    make bgm

---
+++ Assets/bgm.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bgm : MonoBehaviour
+{
+    public bool DontDestroyEnabled = true;
+    public AudioSource bgm1;
+
+    private bool p = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (p)
+        {
+            if (DontDestroyEnabled)
+            {
+                bgm1.Play();
+                // Sceneを遷移してもオブジェクトが消えないようにする
+                DontDestroyOnLoad(this);
+                p = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
